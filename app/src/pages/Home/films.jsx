a8f382f1951cd7b167f53c9d82ddb53f15153163
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-03-18 20:25:36 -0300

    initial

--- app/src/pages/Home/films.jsx
+++ app/src/pages/Home/films.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import Loading from "../../components/Loading";
 
 export default function Films() {
+  document.title = "Star Wars - Films";
+
+  const [data, setData] = useState([]);
+
+  // Estado do loading...
+  const [loading, setLoading] = useState(false);
+
+  const fecthData = async (url) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setData(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    // Url da API
+    const films = "https://swapi.dev/api/films";
+    fecthData(films);
+  }, []);
+
+  console.log(data);
+
+  if (loading) return <Loading />;
+
   return (
     <>
-      <div>
-        <h1>Films</h1>
-      </div>
+    <Header/>
+    {data.map((film) => (
+      <div>{film.title}</div>
+    ))}
     </>
   );
 }
