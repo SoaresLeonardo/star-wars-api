989e45bbe1fb8be417413bb46f14ace3498bb199
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-03-19 10:09:08 -0300

    config Home

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes as Switch,
@@ -6,11 +7,43 @@ import {
 import Home from "./pages/Home/index";
 
 export default function App() {
+  // Lista dos filmes
+  const [dataFilms, setDataFilms] = useState([]);
+
+  // URL base das imagens
+  const image_url = "https://starwars-visualguide.com/assets/img/films/";
+
+  // Estado do loading...
+  const [loading, setLoading] = useState(false);
+
+  // Função que realiza o FECTH
+  const fecthData = async (url) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setDataFilms(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const films = "https://swapi.dev/api/films";
+    fecthData(films);
+  }, []);
+
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <Home loading={loading} data={dataFilms} imageURL={image_url} />
+            }
+          />
         </Switch>
       </Router>
     </>
--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -1,40 +1,15 @@
-import { useEffect, useState } from "react";
 import Films from "../../components/Films";
-
 import Header from "../../components/Header";
 import Loading from "../../components/Loading";
 import { Container, Content } from "./styled";
 
-export default function Home() {
+                            // Destructuring
+export default function Home({ loading, data, imageURL }) {
   document.title = "Star Wars - Films";
-
-  const [data, setData] = useState([]);
-
-  const image_url = "https://starwars-visualguide.com/assets/img/films/";
-
-  // Estado do loading...
-  const [loading, setLoading] = useState(false);
-
-  const fecthData = async (url) => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setLoading(false);
-      setData(data.results);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    // Url da API
-    const films = "https://swapi.dev/api/films";
-    fecthData(films);
-  }, []);
 
   console.log(data);
 
+  // Tela de carregamento
   if (loading) return <Loading />;
 
   return (
@@ -42,7 +17,7 @@ export default function Home() {
       <Header />
       <Container>
         <Content>
-          <Films data={data} img={image_url} />
+          <Films data={data} img={imageURL} />
         </Content>
       </Container>
     </>
